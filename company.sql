20ebfe44927d2d0612c0b6175a5cf7e5c342fd51
Aditya Seshadri <aditya.seshadri.ra@gmail.com>
2020-11-06 14:56:19 -0500

    First Commit

---
+++ company.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS company_DB;
+CREATE DATABASE company_DB;
+
+USE company_DB;
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE position(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+)
